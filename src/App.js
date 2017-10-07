d1dcb0000e0a86f44fbc80434966884030fe6222
Daniel Herrera <admin@digammastudio.com>
2017-10-06 19:37:55 -0500

    Google Login and Logout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import firebase from 'firebase';
 import './App.css';
 
 class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      user: null
+    };
+    this.handleAuth = this.handleAuth.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  componentWillMount (){
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({
+        user
+      });
+    });
+  }
+
+  handleAuth () {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider)
+      .then(result => console.log(`${result.user.email} ha iniciado sesión`))
+      .catch(error => console.log(`Error ${error.code}: ${error.message}`))
+  }
+
+  handleLogout () {
+
+    firebase.auth().signOut()
+      .then(result => console.log(`${result.user.email} ha cerrado sesión`))
+      .catch(error => console.log(`Error ${error.code}: ${error.message}`))
+  }
+
+  renderLoginButton () {
+    if (this.state.user) {
+      return (
+        <div>
+          <img src={this.state.user.photoURL} alt={this.state.user.displayName} />
+          <p>Hola {this.state.user.displayName}!</p>
+          <button onClick={this.handleLogout}>Cerrar Sesión</button>
+        </div>
+      );
+    }else{
+      return (
+        <button onClick={this.handleAuth}>Login con Google</button>
+      )
+    }
+  }
   render() {
     return (
       <div className="App">
@@ -10,7 +57,7 @@ class App extends Component {
           <h1 className="App-title">CloneGram</h1>
         </header>
         <p className="App-intro">
-         <button onClick={this.handleAuth}>Login con Google</button>
+          { this.renderLoginButton() }
         </p>
       </div>
     );
